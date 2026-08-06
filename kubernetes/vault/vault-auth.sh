95ba9794b29ce8e4f6aa4de10cfcd40a385dccf1
Madhuri Chennupati <madhurichennupati010@gmail.com>
2026-08-06 18:08:17 +0530

    updated vault-auth.sh

--- kubernetes/vault/vault-auth.sh
+++ kubernetes/vault/vault-auth.sh
@@ -5,4 +5,4 @@ vault auth enable kubernetes
 vault write auth/kubernetes/config \
   token_reviewer_jwt="$TOKEN" \
   kubernetes_host="https://192.168.49.2:8443" \
-  kubernetes_ca_cert=@ca.crt
+  kubernetes_ca_cert=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
